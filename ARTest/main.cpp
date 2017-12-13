28f4d1b7a992ffb17a4b3a28093e7143d1f00f8c
neconeconeco <1270775934@qq.com>
2017-12-13 19:16:11 +0800

    Initial Commit

---
+++ ARTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ARTest
+//
+//  Created by 郭子乐 on 2017/12/13.
+//  Copyright © 2017年 郭子乐. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
